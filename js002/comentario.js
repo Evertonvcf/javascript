35545efb0104682435e136641c9654033690ac8d
EVERTON  FRANÇA <evertonvcf@gmail.com>
2023-02-22 21:04:43 -0300

    Initial commit

---
+++ js002/comentario.js
@@ -0,0 +1,3 @@
+console.log('Bem vindos ao starter')
+// Aqui eu tenho um comentario em linha \\
+/* Para comentar em varias linhas use barra asterisco para fechar use barra asterisco*/
---
+++ js003/object.js
@@ -0,0 +1,16 @@
+let name = 'Everton';
+let age =32;
+let star =5.1;
+let Issubscribed = true;
+
+let student ={
+    name : 'Everton',
+    age :32,
+    star :5.1,
+    IsSubscribed : true
+}
+let studants 
+studants = [
+    student
+    
+]
