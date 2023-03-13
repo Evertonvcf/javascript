5b8bf8aead5c9522d6eb29f7763ff48e7d8c1ec9
EVERTON  FRANÇA <evertonvcf@gmail.com>
2023-03-13 12:54:50 -0300

    estudos dia 13/03

--- js001/intro.js
+++ js001/intro.js
@@ -1,4 +1,7 @@
-console.log('Bem vindo ao starter')
+//console.log('Bem vindo ao starter')
 window.alert('Hello, world !')
-window.confirm('Está gostando do Js');
-window.prompt('Qual é o seu nome ? ')
+//window.confirm('Está gostando do Js');
+//window.prompt('Qual é o seu nome ? ')
+let name = window.prompt('Qual seu nome ?')
+window.alert("é um grande prazer te conhecer," + name)
+
