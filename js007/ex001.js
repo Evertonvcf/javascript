64cded37946d6c6090a5be4e89ac9ec926b8c8d3
EVERTON  FRANÇA <evertonvcf@gmail.com>
2023-03-15 18:43:14 -0300

    atualizando  os estudos de js

---
+++ js007/ex001.js
@@ -0,0 +1,7 @@
+
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel >60){
+    console.log('você ultrapassoou a velocidade permitida. multado')//condição simples
+}
+console.log('dirija sempre usando sinto de segurança')
---
+++ js007/ex002.js
@@ -0,0 +1,7 @@
+var pais = 'Londres'
+console.log(`vivendo no ${pais},você é `)
+if (pais == 'Brasil') {
+    console.log('Você é Brasileiro')
+}else{
+    console.log('Você é Estrangeiro')
+}
