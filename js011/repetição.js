a0254a79dfad5e655dfaa320c2a4576c91e33e5d
EVERTON  FRANÇA <evertonvcf@gmail.com>
2023-03-22 19:09:16 -0300

    estrutura while

--- js011/repetição.js
+++ js011/repetição.js
@@ -1,7 +1,9 @@
-var cont =1
-while (cont < 6) {
-    console.log('tudo bem !')
-    cont ++
+    var cont =1
+     do {console.log(`tudo bem ! passo ${cont}`)
+    cont ++ // significa cont = cont +1
+     } while (cont <= 7)
 
-}
-
+var Everton = 10
+do {console.log('olá '+ Everton )
+Everton --
+}while (Everton >= 1)
