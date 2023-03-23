421b84e68db99261f5e33e5e9705cc17e2b45192
EVERTON  FRANÇA <evertonvcf@gmail.com>
2023-03-23 19:58:33 -0300

    resolução do exercício

--- js011/script.js
+++ js011/script.js
@@ -3,19 +3,25 @@ function verificar (){
     let finish = document.querySelector('#finish')
     let pass = document.querySelector('#pass')
     let res = document.querySelector('#res')
-    
     if (start.value.length == 0 || finish.value.length == 0 || pass.value.length == 0 ){
         window.alert('Insira um dado válido')
-    }else {
+    } else {
         res.innerHTML = `Contando: `
         let startN = Number(start.value)
         let finishN = Number(finish.value)
         let passN = Number(pass.value)
-
+        if(passN <= 0){
+            window.alert('Passo invalido')
+            passN = 1
+        }
+        if (startN < finishN) {
         for(let c = startN; c <= finishN; c += passN) {
-            res.innerHTML += `${c}`
-        
-
+            res.innerHTML += ` ${c} \u{1f449}`
+            }
+        } else {
+            for(let c = startN; c >= finishN; c -= passN) {
+                res.innerHTML += ` ${c} \u{1f449}`
+            }
         }
     }
 }
