689a344ddd8df0ab23958b6933ae1ec04083ccbc
EVERTON  FRANÇA <evertonvcf@gmail.com>
2023-03-23 16:21:43 -0300

    estrutura for

---
+++ js011/script.js
@@ -0,0 +1,9 @@
+var start = document.querySelector('div#start')
+var finish = document.querySelector('div#finish')
+var pass = document.querySelector
+('div#pass')
+var cont = start.value
+var res =documnt.querySelector('div#res')
+for (var cont; cont <= fim % 1; cont++) {
+    res.innerHtml  = (cont)
+}
