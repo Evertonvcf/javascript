890dd1f1ef73a96a1afbb7b08751faced5bc6b10
EVERTON  FRANÇA <evertonvcf@gmail.com>
2023-03-23 17:32:31 -0300

    concluindo exercício

--- js011/script.js
+++ js011/script.js
@@ -1,9 +1,21 @@
-var start = document.querySelector('div#start')
-var finish = document.querySelector('div#finish')
-var pass = document.querySelector
-('div#pass')
-var cont = start.value
-var res =documnt.querySelector('div#res')
-for (var cont; cont <= fim % 1; cont++) {
-    res.innerHtml  = (cont)
+function verificar (){
+    let start = document.querySelector('#start')
+    let finish = document.querySelector('#finish')
+    let pass = document.querySelector('#pass')
+    let res = document.querySelector('#res')
+    
+    if (start.value.length == 0 || finish.value.length == 0 || pass.value.length == 0 ){
+        window.alert('Insira um dado válido')
+    }else {
+        res.innerHTML = `Contando: `
+        let startN = Number(start.value)
+        let finishN = Number(finish.value)
+        let passN = Number(pass.value)
+
+        for(let c = startN; c <= finishN; c += passN) {
+            res.innerHTML += `${c}`
+        
+
+        }
+    }
 }
