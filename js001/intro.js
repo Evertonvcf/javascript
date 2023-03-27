147423ee64053b685f5f339f67ecede831812e4c
EVERTON  FRANÇA <evertonvcf@gmail.com>
2023-03-27 10:16:28 -0300

    aprendendo arraays

--- js001/intro.js
+++ js001/intro.js
@@ -1,7 +1,10 @@
 //console.log('Bem vindo ao starter')
-window.alert('Hello, world !')
+//window.alert('Hello, world !')
 //window.confirm('Está gostando do Js');
 //window.prompt('Qual é o seu nome ? ')
-let name = window.prompt('Qual seu nome ?')
-window.alert("é um grande prazer te conhecer," + name)
-
+//let name = window.prompt('Qual seu nome ?')
+//window.alert("é um grande prazer te conhecer," + name)
+let num =[1,22,3,4,5,]
+for( let pos in num  ){
+    console.log(`a variavel ${num[pos]} recebe a posiçao${pos}`)
+}
