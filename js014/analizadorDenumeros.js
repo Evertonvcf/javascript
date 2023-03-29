d0c4f21095c1592e31e94700d0d98ca89253315d
EVERTON  FRANÇA <evertonvcf@gmail.com>
2023-03-29 16:32:55 -0300

    ainda tentando

--- js014/analizadorDenumeros.js
+++ js014/analizadorDenumeros.js
@@ -1,3 +1,17 @@
-function adicionar(){
-    let num = w/
+   let num = document.querySelector('input#txtn')
+   let numAdd= document.querySelector('select#numAdd')
+   let res = document.querySelector('div#res')
+   let valores =[]
+   function isNUmber(n) {
+    if
+   }
+   function inListe(n,l) {
+    if
+   }
+   function adicionar() {
+    if (isNumber(num.value) && inListe(num.value, valores)){
+
+    } else {
+        window.alert('O número digitado não é válido ou já foi adicionado a lista')
+    }
 }
