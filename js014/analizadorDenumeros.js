845b0a49ad3f9222841818cae88cfaba813d91e3
EVERTON  FRANÇA <evertonvcf@gmail.com>
2023-03-29 19:19:49 -0300

    exercico resolvido

--- js014/analizadorDenumeros.js
+++ js014/analizadorDenumeros.js
@@ -25,9 +25,36 @@
         let item = document.createElement('option')
         item.text =`Valor ${num.value} adicionado`
         numAdd.appendChild(item)
+        res.innerHTML = ''
         } else {
         window.alert('O número digitado não é válido ou já foi adicionado a lista')
         }
         num.value =''
         num.focus()
+    } 
+
+    function finish() {
+        if (valores == 0) {
+            window.alert('Adicione valores antes de finalizar!')
+        } else {
+            let tot = valores.length
+            let maior = valores[0]
+            let menor =valores[0]
+            let soma = 0
+            let media = 0
+            for(let pos in valores) {
+                soma += valores[pos]
+                if (valores[pos] > maior)
+                maior = valores[pos]
+                if(valores[pos] < menor)
+                menor= valores[pos]
+            }
+            media = soma/tot
+            res.innerHTML = ''
+            res.innerHTML += `<p>Ao todo, temos ${tot}NÚMEROS cadastrados</p>`
+            res.innerHTML += `<p> O maior valor informado foi${maior}</p>`
+            res.innerHTML += `<p> O maior valor informado foi${menor}</p>`
+            res.innerHTML += `<p>A soma dos valores informados foram${soma}</p>`
+            res.innerHTML += `<p>A media  dos valores informados é ${media}</p>`
+        }
     }
