f1080c0beb5f9023c07f390dcb69d2aa4dbe0a2f
EVERTON  FRANÇA <evertonvcf@gmail.com>
2023-03-29 17:14:18 -0300

    agora vai

--- js014/analizadorDenumeros.js
+++ js014/analizadorDenumeros.js
@@ -1,17 +1,33 @@
    let num = document.querySelector('input#txtn')
    let numAdd= document.querySelector('select#numAdd')
    let res = document.querySelector('div#res')
-   let valores =[]
-   function isNUmber(n) {
-    if
-   }
-   function inListe(n,l) {
-    if
-   }
-   function adicionar() {
-    if (isNumber(num.value) && inListe(num.value, valores)){
+   let valores = []
 
-    } else {
-        window.alert('O número digitado não é válido ou já foi adicionado a lista')
+   function isNumber(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+        } else {
+        return false
+        }
+    } 
+
+   function inListe(n, l) {
+    if (l.indexOf(Number(n)) != -1){
+        return true 
+        } else {
+        return false
+        }
     }
-}
+
+   function adicionar() {
+    if (isNumber(num.value) && !inListe(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text =`Valor ${num.value} adicionado`
+        numAdd.appendChild(item)
+        } else {
+        window.alert('O número digitado não é válido ou já foi adicionado a lista')
+        }
+        num.value =''
+        num.focus()
+    }
